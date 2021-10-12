4fb39c3764658aec6e10b1f154c35e61aed9cd9c
Jayleaf1125 <Jasonpadilla104@gmail.com>
2021-10-12 14:01:00 +0000

    code challenge 06 completed

--- index.js
+++ index.js
@@ -1,20 +1,49 @@
 //1
 function sumOfThreeAndFive(){
-  
+    let sum = 0;
+  for(let i = 1; i <= 1000; i++){
+      if(i % 3 === 0 && i % 5 === 0) {
+          sum += i
+      }
+  }
+ return sum;
 }
 
-function greaterNum(){
-  
+function greaterNum(num1, num2){
+  if(num1 > num2) {
+      return num1;
+  } else if (num1 < num2) {
+      return num2;
+  } else if (num1 === num2) {
+      return "both integers are equal"
+  } else {
+      return null;
+  }
 }
 
 function oddAndEvenInFifteen(){
-  
+  for(let i = 0; i <= 15; i++){
+      if (i % 2 === 0) {
+          console.log(`${i} is even`);
+      } else {
+          console.log(`${i} is odd`);
+      }
+  }
 }
 
 function sortThreeNums(){
   
 }
 
+
+// Still Working On
+// sortThreeNums()
+
+
+// Done
+console.log(sumOfThreeAndFive());
+// console.log(greaterNum("50", 50));
+// oddAndEvenInFifteen();
 
 
 
